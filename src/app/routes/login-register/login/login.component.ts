b24313e3714b81cded3a1119795b7c81329d24c4
Junxi <junxi@cxisoftware.com.au>
2021-02-04 23:31:29 +1100

    add style color

--- src/app/routes/login-register/login/login.component.ts
+++ src/app/routes/login-register/login/login.component.ts
@@ -1,4 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from "@app-services/auth/auth.service";
+import { ApplicationService } from "@app-services/app/application.service";
+import { LoginModel } from '@app-models/user';
 
 @Component({
   selector: 'login-form',
@@ -7,9 +13,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  errors: any[] = [];
+  loginForm = this.fb.group({
+    email : new FormControl('',[Validators.required,Validators.email]),
+    password : new FormControl('',[Validators.required])
+  })
+  
+  submitDisable: boolean = false;
+
+   constructor(private route:Router, private auth:AuthService, private fb:FormBuilder, private app:ApplicationService) { }
+
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.errors = [];
+    if(this.loginForm.valid){
+      const loginForm : LoginModel = {
+        email: this.loginForm.get('email').value,
+        password: this.loginForm.get('password').value
+      };
+      this.submitDisable = true;
+      this.app.openSnackBar('Logging to your account..', 'loading', 120000);
+      this.auth.login(loginForm).subscribe(x => {
+        this.submitDisable = false;
+        this.app.openSnackBar('Welcome to AOL Leagues', 'success');
+        this.route.navigate(['me']);
+      }, (err: HttpErrorResponse) => {
+        this.submitDisable = false;
+        this.app.errorHandler(err);
+      })
+    } else {
+      let errorList = this.app.formErrorHandler(this.loginForm);
+      errorList.forEach(error => { 
+          this.errors.push(LoginModel.getErrorMessage(error.field,error.error))
+      })
+    }
+    
+  }
+  
 }
