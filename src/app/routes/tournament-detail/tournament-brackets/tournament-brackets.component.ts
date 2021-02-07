3464d1075ba494e7cf17374929906b740297a8fb
Junxi <junxi@cxisoftware.com.au>
2021-02-07 20:48:16 +1100

    wait to fix combineFixed

--- src/app/routes/tournament-detail/tournament-brackets/tournament-brackets.component.ts
+++ src/app/routes/tournament-detail/tournament-brackets/tournament-brackets.component.ts
@@ -2,6 +2,16 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { bracketDTO, BracketGroupDTO } from '@app-models/tournament';
 import { TournamentDetailStateService } from '@app-services/state/tournament-detail-state.service';
 import { trigger, state, style, animate, transition } from '@angular/animations';
+import { BehaviorSubject, combineLatest, zip } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ApplicationService } from '@app-services/app/application.service';
+import { AuthService } from '@app-services/auth/auth.service';
+
+enum BracketFilter{
+  all = 1,
+  me = 2
+}
+
 @Component({
   selector: 'app-tournament-brackets',
   templateUrl: './tournament-brackets.component.html',
@@ -27,13 +37,39 @@ export class TournamentBracketsComponent implements OnInit , AfterViewInit{
 
   bracketSelected: number = 0;
   currentBrackets: bracketDTO[] = [];
-  constructor(public state: TournamentDetailStateService) {
-    this.state.brackets$.subscribe(res => { 
+  bracketFilter$ = new BehaviorSubject<BracketFilter>(1);
+  constructor(public state: TournamentDetailStateService, public auth:AuthService) {
+
+    combineLatest([this.bracketFilter$, this.state.brackets$, this.auth.currentUserSubject]).pipe(map(x => { 
+      //debugger;
+      let filter: BracketFilter = x[0];
+      let brackets = Object.assign([],[...x[1]]);
+      let myDetail = Object.assign({},x[2]);
+      if (brackets.length > 0 && brackets && myDetail && myDetail.user.id) {
+        let my_team_id = myDetail.user.team.id;
+        if (filter == BracketFilter.me) {
+          brackets.forEach(brk => { 
+            let allBrackets = [...brk.brackets];
+            brk.brackets = [];
+            for (let y = 0; y < brk.brackets.length; y++){
+              if (allBrackets[y].teamA == my_team_id || allBrackets[y].teamB == my_team_id) {
+                brk.brackets.push(brk.brackets[y]);
+              }
+            }
+          })
+  
+          return brackets;
+        } else {
+          return brackets;
+        }
+      }
+      return [];
+    })).subscribe(res => { 
       if (res && res.length > 0) {
         this.bracketSelected = res[0].bracketKnockoutId;
-        this.currentBrackets = res[0].brackets;
+        this.currentBrackets = [...res[0].brackets];
       }
-     
+
     })
   }
 
@@ -48,5 +84,10 @@ export class TournamentBracketsComponent implements OnInit , AfterViewInit{
     this.currentBrackets = [];
     this.bracketSelected = brackets[0].bracketKnockoutId;
     this.currentBrackets = brackets;
+  }
+
+  switchBracketFilter(filter:any) {
+    console.log(filter.target.value);
+    this.bracketFilter$.next(parseInt(filter.target.value));
   }
 }
