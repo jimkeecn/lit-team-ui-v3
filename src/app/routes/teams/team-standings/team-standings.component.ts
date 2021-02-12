16145d3074046ffa1429108e23bcdc35848f4d5c
Junxi <junxi@cxisoftware.com.au>
2021-02-12 15:30:49 +1100

    add team search

--- src/app/routes/teams/team-standings/team-standings.component.ts
+++ src/app/routes/teams/team-standings/team-standings.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { TeamViewModel } from '@app-models/user';
+import { debounceTime, distinctUntilChanged, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-team-standings',
@@ -9,12 +11,27 @@ import { TeamViewModel } from '@app-models/user';
 })
 export class TeamStandingsComponent implements OnInit {
 
+  teamSearch = new FormControl("");
+  allTeams: TeamViewModel[] = [];
   _teams: TeamViewModel[] = [];
   constructor(private route: ActivatedRoute) {
     this._teams = this.route.snapshot.data.detail;
+    this.allTeams = this.route.snapshot.data.detail;
    }
 
   ngOnInit(): void {
+    this.teamSearch.valueChanges.pipe(distinctUntilChanged(),debounceTime(500),map(x => { 
+      debugger;
+      let teams = [];
+      for (let t = 0; t < this.allTeams.length; t++){
+        if (this.allTeams[t].name.toLowerCase().includes(x.toLowerCase())) {
+          teams.push(this.allTeams[t])
+        }
+      }
+      return teams;
+    })).subscribe(res => { 
+      this._teams = res;
+    })
   }
 
 }
