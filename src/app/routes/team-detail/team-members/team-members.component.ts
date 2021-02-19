d7ef8d670706f35101a698539f23899ce12c44d9
Junxi <junxi@cxisoftware.com.au>
2021-02-19 18:12:18 +1100

    add slide show for members of the team.

--- src/app/routes/team-detail/team-members/team-members.component.ts
+++ src/app/routes/team-detail/team-members/team-members.component.ts
@@ -1,4 +1,5 @@
 import { animate, style, transition, trigger } from '@angular/animations';
+import { Xmb } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { TeamDetailStateService } from '@app-services/state/team-detail-state.service';
 
@@ -25,13 +26,43 @@ import { TeamDetailStateService } from '@app-services/state/team-detail-state.se
 })
 export class TeamMembersComponent implements OnInit {
   selectedId: number = 0;
+  memberIds: number[] = [];
   constructor(public state: TeamDetailStateService) { 
     this.state.overview$.subscribe(x => { 
       this.selectedId = x.members[0].id;
+      if (x.members.length > 0) {
+        x.members.forEach(m => { 
+          this.memberIds.push(m.id);
+        })
+      }
+      
     })
   }
 
   ngOnInit(): void {
+    if (this.memberIds.length > 0) {
+      setInterval(() => { 
+        let nextId = this.memberIds.findIndex(x => x == this.selectedId) !== null
+          && this.memberIds.findIndex(x => x == this.selectedId) < this.memberIds.length - 1
+          ? this.memberIds.findIndex(x => x == this.selectedId) + 1 : 0;
+        console.log(nextId);
+        this.selectedId = this.memberIds[nextId];
+
+
+        /**** easy logic here  ***** */
+        
+        // let currentIndex = this.memberIds.findIndex(x => x == this.selectedId);
+        // if (currentIndex < this.memberIds.length - 1) {
+        //   this.selectedId = this.memberIds[currentIndex + 1];
+        //   console.log(this.memberIds[currentIndex + 1])
+        // } else {
+        //   this.selectedId = this.memberIds[0];
+        //   console.log(this.memberIds[0]);
+        // }
+
+      },5000)
+    }
+    
   }
 
 }
