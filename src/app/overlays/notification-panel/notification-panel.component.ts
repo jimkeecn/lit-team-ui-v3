3250504779a3ab01fb94545231385f63cb9d88f8
Junxi <junxi@cxisoftware.com.au>
2021-02-25 11:32:48 +1100

    fixing error code

--- src/app/overlays/notification-panel/notification-panel.component.ts
+++ src/app/overlays/notification-panel/notification-panel.component.ts
@@ -15,24 +15,6 @@ export class NotificationPanelComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  accept(id) {
-    console.log(id);
-    this.api.acceptApplication(id).subscribe(res => { 
-      this.app.openSnackBar(`Start journey with your clan mates!`, 'success');
-      this.app.removeFromInvitations(id);
-    }, (err: HttpErrorResponse) => {
-      this.app.errorHandler(err);
-    })
-  }
 
-  reject(id) {
-    console.log(id);
-    this.api.denialApplication(id).subscribe(res => { 
-      this.app.openSnackBar(`You rejected an invitation`, 'success');
-      this.app.removeFromInvitations(id);
-    }, (err: HttpErrorResponse) => {
-      this.app.errorHandler(err);
-    })
-  }
 
 }
