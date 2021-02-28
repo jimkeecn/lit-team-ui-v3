0013cc2c85384dfd677ec3062b5d7cb1341f416a
Junxi <junxi@cxisoftware.com.au>
2021-02-28 17:27:37 +1100

    complete match state

--- src/app/routes/match-detail/match-states/match-states.component.ts
+++ src/app/routes/match-detail/match-states/match-states.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { ApiService } from '@app-services/api/api.service';
 import { TourneyApiService } from '@app-services/api/tourney-api.service';
 import { ApplicationService } from '@app-services/app/application.service';
 import { TournamentDetailStateService } from '@app-services/state/tournament-detail-state.service';
@@ -13,8 +14,9 @@ export class MatchStatesComponent implements OnInit {
 
   @Input() matches = [];
   @Input() tournamentCodes = [];
+  file: any;
   constructor(public state: TournamentDetailStateService,
-    public api: TourneyApiService,
+    public api: ApiService,
     public app: ApplicationService,
     public dc:ChangeDetectorRef) { }
 
@@ -23,6 +25,38 @@ export class MatchStatesComponent implements OnInit {
 
   copyTournamentCode() {
     this.app.openSnackBar("You have copied the code","success")
+  }
+
+  onFileChange(event) {
+    if (event.target.files.length > 0) {
+      this.file = event.target.files[0];
+    }
+  }
+
+  private prepareSave(matchId): any {
+    let input = new FormData();
+    input.append('replay', this.file);
+    input.append('matchId',matchId)
+    return input;
+  }
+
+  submitDisable: boolean = false;
+  upload(matchId) {
+    if (this.file) {
+      const param = this.prepareSave(matchId);
+      this.submitDisable = true;
+      this.app.openSnackBar('Uploading your match replay..', 'loading', 120000);
+      this.api.updateMatchResult(param).subscribe(res=>{
+        this.app.openSnackBar(`You have upload your replay.`, 'success');
+        this.submitDisable = false;
+      }, (err: HttpErrorResponse) => {
+        this.submitDisable = false;
+        this.app.errorHandler(err);
+      })
+    } else {
+      this.app.openSnackBar(`You must select your file.`, 'error');
+    }
+   
   }
 
 }
